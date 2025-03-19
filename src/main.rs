14169618a2112513440b30e49d9fa360a40fe769
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-03-19 09:16:10 +0530

    just small change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let num: u8 = 46;
+    let str_value: String = String::from("non changeable variable");
+
+    println!(" {}", str_value);
+    let str_value = "new change";
+    println!("Hello, world! {}", num);
+    println!(" {}", str_value);
 }
