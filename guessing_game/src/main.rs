8b43d3db0a1af83cdf99ec7032b49a524c5ef577
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-04-07 10:09:38 +0530

    init guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+
+fn main() {
+    println!("guessing Game");
+
+	println!("enter the number");
+
+	let mut guess = String::new(); //byDefault it is immutable
+
+	io::stdin()
+
+}
---
+++ guessing_game/target/debug/deps/guessing_game-b486f79729d2a30b.d
@@ -0,0 +1,5 @@
+D:\Rust\guessing_game\target\debug\deps\guessing_game-b486f79729d2a30b.exe: src\main.rs
+
+D:\Rust\guessing_game\target\debug\deps\guessing_game-b486f79729d2a30b.d: src\main.rs
+
+src\main.rs:
