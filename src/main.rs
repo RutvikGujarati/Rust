b0cc97a24a4e5bec36f2f1544ffb522135034a88
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2025-07-01 09:05:29 +0530

    stack example(integer)

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,26 @@
 //     return s;
 // }
 
-// heap memory 
+//------------------- heap memory
+
+// fn main() {
+//     let s1 = String::from("Hello, world!"); // current owner is s1 of Hello world
+//     fn_str_pass(s1); // transfer to second function
+//     println!("s1 printing{}", s1); // gives error cause no owner of hello world at this point
+// }
+
+// fn fn_str_pass(value: String) {
+//     println!("s2 printing..{}", value);
+// }
+
+// ------------ stack memory
 
 fn main() {
-    let s1 = String::from("Hello, world!"); // current owner is s1 of Hello world
-    fn_str_pass(s1); // transfer to second function
-    println!("s1 printing{}", s1); // gives error cause no owner of hello world at this point
+    let integer: u8 = 34;
+    pass_integer(integer);
+    println!("int passing..{}", integer);// this will run successfully cause it works on stack
 }
 
-fn fn_str_pass(value: String) {
-    println!("s2 printing..{}", value);
+fn pass_integer(value:u8) {
+    println!("int passing..{}", value);
 }
